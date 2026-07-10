20d8f33624749632acd1f4e19da1c2f9d9ede42b
R <r@arcanenibble.com>
2026-07-10 06:48:54 +0100

    Fix making release builds

--- tolk2spd_win32/build-release.sh
+++ tolk2spd_win32/build-release.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-cargo build
+cargo build --release
 printf "Wine builtin DLL\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" | dd of=target/x86_64-pc-windows-gnullvm/release/tolk.dll seek=64 bs=1 conv=notrunc
