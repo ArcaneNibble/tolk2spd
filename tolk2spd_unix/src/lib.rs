6d3d8a58563e76865856bd17304cdf71300347ac
R <r@arcanenibble.com>
2026-07-10 05:52:35 +0100

    Set SSIP client name

--- tolk2spd_unix/src/lib.rs
+++ tolk2spd_unix/src/lib.rs
@@ -1,9 +1,10 @@
 use std::ffi::c_void;
-use std::io::Write;
+use std::io::{self, BufRead, BufReader, Write};
 use std::os::unix::net::UnixStream;
 use std::path::PathBuf;
 
 mod ffi;
+mod whoami;
 
 extern "C" fn get_version(_arg: *const c_void) -> u32 {
     tolk2spd_abi::ABI_VERSION
@@ -15,7 +16,7 @@ pub struct SPDConnection {
 }
 
 impl SPDConnection {
-    pub fn new(_exename: &str) -> Option<Self> {
+    pub fn new(exename: &str) -> Option<Self> {
         // Try to find the socket path
         let mut sock_path = if let Some(x) = std::env::var_os("XDG_RUNTIME_DIR") {
             PathBuf::from(x)
@@ -34,12 +35,49 @@ impl SPDConnection {
         dbg!(&sock_path);
 
         // Try to connect to the socket
-        let mut stream = UnixStream::connect(&sock_path).ok()?;
-        dbg!(&stream);
+        let stream = UnixStream::connect(&sock_path).ok()?;
+        let mut ret = Self { stream };
 
-        write!(stream, "speak\r\nthis is a test\r\n.\r\n").ok()?;
+        // Send the client name
+        let username = whoami::get_username();
+        let result = ret
+            .send_cmd(&format!(
+                "set self client_name {username}:{exename}:tolk2spd"
+            ))
+            .ok()?;
+        if !(result.0 >= 200 && result.0 <= 299) {
+            eprintln!("warn: setting ssip client_name failed! {}", result.1);
+        }
 
-        Some(Self { stream })
+        Some(ret)
+    }
+
+    fn send_cmd(&mut self, cmd: &str) -> io::Result<(u32, String)> {
+        write!(self.stream, "{}\r\n", cmd)?;
+
+        let mut response_code = None;
+        let mut response_msg = String::new();
+        let mut r = BufReader::new(&self.stream);
+        loop {
+            let start_idx = response_msg.len();
+            let nbytes = r.read_line(&mut response_msg)?;
+            if nbytes == 0 {
+                return Err(io::Error::from(io::ErrorKind::UnexpectedEof));
+            }
+
+            let l = &response_msg[start_idx..start_idx + nbytes];
+            dbg!(l);
+
+            if response_code.is_none() {
+                response_code = Some(l[..3].parse::<u32>().unwrap_or_default());
+            }
+
+            if l.as_bytes()[3] != b'-' {
+                break;
+            }
+        }
+
+        Ok((response_code.unwrap_or_default(), response_msg))
     }
 }
 
---
+++ tolk2spd_unix/src/whoami.rs
@@ -0,0 +1,40 @@
+use std::alloc::Layout;
+use std::ffi::CStr;
+use std::mem::MaybeUninit;
+use std::ptr;
+
+use libc::*;
+
+pub fn get_username() -> String {
+    unsafe {
+        let uid = geteuid();
+
+        let pwdb_max_sz = sysconf(_SC_GETPW_R_SIZE_MAX);
+        if pwdb_max_sz < 0 {
+            return format!("{}", uid);
+        }
+        let pwdb_max_sz = pwdb_max_sz as usize;
+        let buf_layout = Layout::from_size_align(pwdb_max_sz, 1).unwrap();
+
+        let pwdb_buf = std::alloc::alloc_zeroed(buf_layout);
+        let mut passwd = MaybeUninit::<passwd>::zeroed().assume_init();
+        let mut _useless = ptr::null_mut();
+        let ret = getpwuid_r(
+            uid,
+            &mut passwd,
+            pwdb_buf as *mut c_char,
+            pwdb_max_sz,
+            &mut _useless,
+        );
+        if ret < 0 {
+            return format!("{}", uid);
+        }
+
+        let username = CStr::from_ptr(passwd.pw_name);
+        let ret = username.to_string_lossy().into_owned();
+
+        std::alloc::dealloc(pwdb_buf, buf_layout);
+
+        ret
+    }
+}
